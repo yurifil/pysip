a0d2154703bf154f9e9241b88eb3efcc7a3b8893
Yury Fil <yury.fil@nordigy.ru>
2019-01-28 14:56:31 +0300

    Add callid module

---
+++ test/message/test_hdr_callid.py
@@ -0,0 +1,25 @@
+import pytest
+from pysip.message.hdr import Header
+from pysip.binary import to_bytes
+from pysip.message.hdr_callid import CallIDError, CallIDHeader, WORD_CHARS
+import string
+
+
+def make_alphanum_string():
+    return string.ascii_lowercase + string.ascii_uppercase + string.digits
+
+
+@pytest.mark.parametrize('value', [b'a@b',
+                                   to_bytes(WORD_CHARS + make_alphanum_string() + '@' + make_alphanum_string() +
+                                            WORD_CHARS),
+                                   to_bytes(WORD_CHARS + make_alphanum_string())])
+def test_parse_header_success(value):
+    h = Header(b'Call-ID')
+    h.add_value(value)
+    assert h == CallIDHeader(value)
+
+
+@pytest.mark.parametrize('value', [b',', None, '', b'a@@b', b'a@b, c@d'])
+def test_parse_header_fail(value):
+    with pytest.raises(CallIDError):
+        CallIDHeader(value)
